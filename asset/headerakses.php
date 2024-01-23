b85967789e9392adcba29c0e5e39bb52987a56b0
TwentinineMarsanda <146809645+TwentinineMarsanda@users.noreply.github.com>
2024-01-23 12:44:23 +0800

    Update headerakses.php

--- asset/headerakses.php
+++ asset/headerakses.php
@@ -9,11 +9,11 @@
 
     <title><?= $_SESSION['judul'] ; ?></title>
 
-    <!-- Custom fonts for this template-->
+    
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
 
-    <!-- Custom styles for this template-->
+    
     <link href="css/sb-admin-2.min.css" rel="stylesheet" />
  
   </head>
@@ -22,4 +22,4 @@
     <div class="container col-lg-5">
       <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="card-body p-0">
-          <!-- Nested Row within Card Body -->
+          
