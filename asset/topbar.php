89fc5b4fc41256a7eb568b93d46488e3e121adfc
TwentinineMarsanda <146809645+TwentinineMarsanda@users.noreply.github.com>
2024-01-23 12:45:53 +0800

    Update topbar.php

--- asset/topbar.php
+++ asset/topbar.php
@@ -1,24 +1,24 @@
-<!-- Content Wrapper -->
+
 <div id="content-wrapper" class="d-flex flex-column">
 
-<!-- Main Content -->
+
 <div id="content">
 
-    <!-- Topbar -->
+   
     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-        <!-- Sidebar Toggle (Topbar) -->
+        
         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
         </button>
 
-        <!-- Topbar Navbar -->
+      
         <ul class="navbar-nav ml-auto">
 
 
             <div class="topbar-divider d-none d-sm-block"></div>
 
-            <!-- Nav Item - User Information -->
+            
             <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -27,7 +27,7 @@
                     <img class="img-profile rounded-circle"
                         src="../img/avatar.svg">
                 </a>
-                <!-- Dropdown - User Information -->
+                
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                     aria-labelledby="userDropdown">
                     <div class="dropdown-divider"></div>
@@ -41,4 +41,4 @@
         </ul>
 
     </nav>
-    <!-- End of Topbar -->
+    
